0f56052ae1360e5e8b033eabaf526da607b8ac40
Arnab Gupta <arnabgupta91996@gmail.com>
2021-08-21 23:36:42 +0530

    new changes

---
+++ Editor/src/main/java/com/threed/editor/LevelEditor.java
@@ -0,0 +1,15 @@
+package com.threed.editor;
+
+import javax.swing.*;
+
+public class LevelEditor {
+    private JPanel panel;
+
+    public LevelEditor() {
+        
+    }
+
+    public JPanel getPanel() {
+        return this.panel;
+    }
+}
---
+++ Editor/src/main/java/com/threed/editor/TestApp.java
@@ -0,0 +1,18 @@
+package com.threed.editor;
+
+public class TestApp {
+    public static void main(String[] args) {
+
+//        LevelManagerService levelManagerService = new LevelManagerServiceImpl();
+//        long window = levelManagerService.createWindow(new WindowFactory(1500, 900, "TEST"));
+//        levelManagerService.updateWindow(window);
+//
+//        JFrame jFrame = new JFrame("EDITOR");
+//        LevelEditor levelEditor = new LevelEditor();
+//        jFrame.setContentPane(levelEditor.getPanel());
+//        levelEditor.getPanel().setUI(window);
+//        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        jFrame.pack();
+//        jFrame.setVisible(true);
+    }
+}
